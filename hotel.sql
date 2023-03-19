40b1e0a099d2b60ecf6acf3c66ff2b4be03361dd
Oleksii Bolotin <samkiral73@gmail.com>
2023-03-20 00:05:30 +0100

    add new table aa intohotel.sql because dont work,script reservation is working

--- hotel.sql
+++ hotel.sql
@@ -5,7 +5,7 @@ SET time_zone = "+00:00";
 
 
 CREATE TABLE Room (
-    room_number VARCHAR(10) NOT NULL,
+    room_number INT NOT NULL,
     room_type VARCHAR(50) NOT NULL,
     room_price DECIMAL(10,2) NOT NULL,
     max_occupancy INT NOT NULL,
@@ -15,28 +15,67 @@ CREATE TABLE Room (
 );
 
 INSERT INTO Room (room_number, room_type, room_price, max_occupancy, available, floor)
-VALUES 
-    ('101', 'Single', 100.00, 1, TRUE, 1),
-    ('102', 'Double', 150.00, 2, TRUE, 1),
-    ('103', 'Suite', 250.00, 4, TRUE, 1),
-    ('104', 'Single', 100.00, 1, TRUE, 1),
-    ('105', 'Double', 150.00, 2, TRUE, 1),
-    ('201', 'Single', 100.00, 1, TRUE, 2),
-    ('202', 'Double', 150.00, 2, TRUE, 2),
-    ('203', 'Suite', 250.00, 4, TRUE, 2),
-    ('204', 'Single', 100.00, 1, TRUE, 2),
-    ('205', 'Double', 150.00, 2, TRUE, 2),
-    ('301', 'Single', 100.00, 1, TRUE, 3),
-    ('302', 'Double', 150.00, 2, TRUE, 3),
-    ('303', 'Suite', 250.00, 4, TRUE, 3),
-    ('304', 'Single', 100.00, 1, TRUE, 3),
-    ('305', 'Double', 150.00, 2, TRUE, 3),
-    ('401', 'Single', 100.00, 1, TRUE, 4),
-    ('402', 'Double', 150.00, 2, TRUE, 4),
-    ('403', 'Suite', 250.00, 4, TRUE, 4),
-    ('404', 'Single', 100.00, 1, TRUE, 4),
-    ('405', 'Double', 150.00, 2, TRUE, 4);
+VALUES
+    (101, 'Single', 100.00, 1, FALSE, 1),
+    (102, 'Double', 150.00, 2, TRUE, 1),
+    (103, 'Suite', 250.00, 4, TRUE, 1),
+    (104, 'Single', 100.00, 1, TRUE, 1),
+    (105, 'Double', 150.00, 2, FALSE, 1),
+    (201, 'Single', 100.00, 1, TRUE, 2),
+    (202, 'Double', 150.00, 2, TRUE, 2),
+    (203, 'Suite', 250.00, 4, FALSE, 2),
+    (204, 'Single', 100.00, 1, TRUE, 2),
+    (205, 'Double', 150.00, 2, TRUE, 2),
+    (301, 'Single', 100.00, 1, TRUE, 3),
+    (302, 'Double', 150.00, 2, TRUE, 3),
+    (303, 'Suite', 250.00, 4, TRUE, 3),
+    (304, 'Single', 100.00, 1, TRUE, 3),
+    (305, 'Double', 150.00, 2, TRUE, 3),
+    (401, 'Single', 100.00, 1, TRUE, 4),
+    (402, 'Double', 150.00, 2, TRUE, 4),
+    (403, 'Suite', 250.00, 4, TRUE, 4),
+    (404, 'Single', 100.00, 1, TRUE, 4),
+    (405, 'Double', 150.00, 2, TRUE, 4);
 
+
+
+
+
+    CREATE TABLE Guests (
+    guest_id INT NOT NULL AUTO_INCREMENT,
+    guest_name VARCHAR(50) NOT NULL,
+    guest_email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    PRIMARY KEY (guest_id)
+);
+
+INSERT INTO Guests (guest_name, guest_email, phone_number, address)
+VALUES 
+    ('John Smith', 'john.smith@example.com', '555-1234', '123 Main St, Anytown USA'),
+    ('Jane Doe', 'jane.doe@example.com', '555-5678', '456 Maple Ave, Anytown USA'),
+    ('David Lee', 'david.lee@example.com', '555-9012', '789 Oak St, Anytown USA'),
+    ('Sarah Johnson', 'sarah.johnson@example.com', '555-3456', '101 Elm St, Anytown USA'),
+    ('Michael Chen', 'michael.chen@example.com', '555-7890', '222 Cedar Ave, Anytown USA'),
+    ('Emily Wilson', 'emily.wilson@example.com', '555-2345', '333 Pine St, Anytown USA'),
+    ('Daniel Kim', 'daniel.kim@example.com', '555-6789', '444 Birch Rd, Anytown USA');
+
+
+CREATE TABLE Reservation (
+    reservation_id INT NOT NULL AUTO_INCREMENT,
+    guest_id INT NOT NULL,
+    room_number INT NOT NULL,
+    check_in_date DATE NOT NULL,
+    check_out_date DATE NOT NULL,
+    PRIMARY KEY (reservation_id),
+    FOREIGN KEY (guest_id) REFERENCES Guests(guest_id),
+    FOREIGN KEY (room_number) REFERENCES Room(room_number)
+);
+
+INSERT INTO Reservation (guest_id, room_number, check_in_date, check_out_date)
+VALUES (5, 101, '2023-03-20', '2023-03-23'),
+    (6, 105, '2023-04-05', '2023-04-08'),
+    (7, 203, '2023-05-01', '2023-05-03');
 
 CREATE TABLE Employee (
     employee_id INT NOT NULL AUTO_INCREMENT,
@@ -57,40 +96,15 @@ VALUES
     ('Michael Chen', 'Maintenance', 'Facilities', 30000.00, '2020-05-01');
 
 
-    CREATE TABLE Guest (
-    guest_id INT NOT NULL AUTO_INCREMENT,
-    guest_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    phone_number VARCHAR(20) NOT NULL,
-    address VARCHAR(100) NOT NULL,
-    PRIMARY KEY (guest_id)
-);
 
-INSERT INTO Guest (guest_name, email, phone_number, address)
-VALUES 
-    ('John Smith', 'john.smith@example.com', '555-1234', '123 Main St, Anytown USA'),
-    ('Jane Doe', 'jane.doe@example.com', '555-5678', '456 Maple Ave, Anytown USA'),
-    ('David Lee', 'david.lee@example.com', '555-9012', '789 Oak St, Anytown USA'),
-    ('Sarah Johnson', 'sarah.johnson@example.com', '555-3456', '101 Elm St, Anytown USA'),
-    ('Michael Chen', 'michael.chen@example.com', '555-7890', '222 Cedar Ave, Anytown USA'),
-    ('Emily Wilson', 'emily.wilson@example.com', '555-2345', '333 Pine St, Anytown USA'),
-    ('Daniel Kim', 'daniel.kim@example.com', '555-6789', '444 Birch Rd, Anytown USA');
-
-
-CREATE TABLE Reservation (
+CREATE TABLE aa (
     reservation_id INT NOT NULL AUTO_INCREMENT,
-    guest_id INT NOT NULL,
-    room_number INT NOT NULL,
     check_in_date DATE NOT NULL,
     check_out_date DATE NOT NULL,
-    PRIMARY KEY (reservation_id),
-    FOREIGN KEY (guest_id) REFERENCES Guest(guest_id),
-    FOREIGN KEY (room_number) REFERENCES Room(room_number)
-);
+    PRIMARY KEY (reservation_id) );
 
-INSERT INTO Reservation (guest_id, room_number, check_in_date, check_out_date)
-VALUES 
-    (1, 101, '2023-03-20', '2023-03-23'),
-    (2, 105, '2023-04-05', '2023-04-08'),
-    (3, 110, '2023-05-01', '2023-05-03');
+INSERT INTO aa (check_in_date, check_out_date) VALUES ('2023-03-20', '2023-03-23'),
+    ('2023-04-05', '2023-04-08'),
+    ('2023-05-01', '2023-05-03');
 
+    -- ---------------------------------------------------
--- rezervation.php
+++ rezervation.php
@@ -4,30 +4,38 @@
 	<title>Hotel Reservation System</title>
 </head>
 <body>
+
 	<?php
 		// Connect to the database
 		$servername = "localhost";
 		$username = "root";
 		$password = "";
-		$dbname = "hotel_db";
+		$dbname = "hotel";
 
 		$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 		if (!$conn) {
+			
 			die("Connection failed: " . mysqli_connect_error());
 		}
-
+		
 		// Check if the user has submitted the reservation form
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			
 			// Retrieve the form data
 			$guest_name = $_POST["guest_name"];
+			
 			$guest_email = $_POST["guest_email"];
 			$room_number = $_POST["room_number"];
+			$phone_number = $_POST["phone_number"];
+			$adress = $_POST["address"];
 			$check_in_date = $_POST["check_in_date"];
 			$check_out_date = $_POST["check_out_date"];
 
 			// Insert the new reservation into the database
-			$sql = "INSERT INTO Reservation (guest_name, guest_email, room_number, check_in_date, check_out_date) VALUES ('$guest_name', '$guest_email', '$room_number', '$check_in_date', '$check_out_date')";
+			$sql = "INSERT INTO Guests (guest_name,guest_email,phone_number,address) VALUES ('$guest_name','$guest_email','$phone_number','$adress')";
+			
+			echo $sql;
 
 			if (mysqli_query($conn, $sql)) {
 				echo "Reservation made successfully";
@@ -36,8 +44,11 @@
 			}
 		}
 
+		
+
+
 		// Query the database for available rooms
-		$sql = "SELECT * FROM Room WHERE is_available = 1";
+		$sql = "SELECT * FROM Room WHERE available = 1";
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) > 0) {
@@ -53,6 +64,15 @@
 				echo "<input type='text' id='guest_name' name='guest_name'><br>";
 				echo "<label for='guest_email'>Email:</label>";
 				echo "<input type='email' id='guest_email' name='guest_email'><br>";
+
+echo "<label for='guest_name'>Phone number:</label>";
+				echo "<input type='text' id='phone_number' name='phone_number'><br>";
+
+				echo "<label for='address'>Address:</label>";
+				echo "<input type='text' id='address' name='address'><br>";
+
+
+				
 				echo "<label for='check_in_date'>Check-in Date:</label>";
 				echo "<input type='date' id='check_in_date' name='check_in_date'><br>";
 				echo "<label for='check_out_date'>Check-out Date:</label>";
@@ -63,73 +83,6 @@
 			echo "</table>";
 		} else {
 			echo "No rooms available";
-		}
-
-		// Close the database connection
-		mysqli_close($conn);
-	?>
-</body>
-</html>
-
-+++++++
-kogda gosti uezgajut Sure, here's an example PHP script to update the database when a guest leaves:
-
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Check Out</title>
-</head>
-<body>
-	<?php
-		// Connect to the database
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "hotel_db";
-
-		$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-		if (!$conn) {
-			die("Connection failed: " . mysqli_connect_error());
-		}
-
-		// Check if the user has submitted the check-out form
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			// Retrieve the form data
-			$room_number = $_POST["room_number"];
-			$check_out_date = $_POST["check_out_date"];
-
-			// Update the room and reservation status in the database
-			$sql = "UPDATE Room SET is_available = 1 WHERE room_number = '$room_number'";
-			mysqli_query($conn, $sql);
-
-			$sql = "UPDATE Reservation SET check_out_date = '$check_out_date' WHERE room_number = '$room_number' AND check_out_date IS NULL";
-			mysqli_query($conn, $sql);
-
-			echo "Check-out completed successfully";
-		}
-
-		// Query the database for active reservations
-		$sql = "SELECT Reservation.reservation_id, Guest.guest_name, Room.room_number, Reservation.check_in_date FROM Reservation INNER JOIN Guest ON Reservation.guest_id = Guest.guest_id INNER JOIN Room ON Reservation.room_number = Room.room_number WHERE Reservation.check_out_date IS NULL";
-		$result = mysqli_query($conn, $sql);
-
-		if (mysqli_num_rows($result) > 0) {
-			// Display the list of active reservations and check-out form
-			echo "<h1>Active Reservations</h1>";
-			echo "<table>";
-			echo "<tr><th>Reservation ID</th><th>Guest Name</th><th>Room Number</th><th>Check-in Date</th><th>Check-out</th></tr>";
-			while ($row = mysqli_fetch_assoc($result)) {
-				echo "<tr><td>" . $row["reservation_id"] . "</td><td>" . $row["guest_name"] . "</td><td>" . $row["room_number"] . "</td><td>" . $row["check_in_date"] . "</td><td>";
-				echo "<form method='post' action='" . $_SERVER["PHP_SELF"] . "'>";
-				echo "<input type='hidden' name='room_number' value='" . $row["room_number"] . "'>";
-				echo "<label for='check_out_date'>Check-out Date:</label>";
-				echo "<input type='date' id='check_out_date' name='check_out_date'>";
-				echo "<input type='submit' value='Check Out'>";
-				echo "</form></td></tr>";
-			}
-			echo "</table>";
-		} else {
-			echo "No active reservations";
 		}
 
 		// Close the database connection
